f6498141f9ee75605bd91aec576d5769579aac0b
Nabila <nabilazulfani11@gmail.com>
2021-11-11 11:50:43 +0700

    permisiion geo location

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
+// perintah aktifin cek lokasi
+function userLocation() {
+  if (!navigator.geolocation) {
+    alert("Browser anda tidak mendukung akses lokasi!");
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
+//lokasi gagal di akses
+function error() {
+  alert("Akes lokasi di tolak !");
+}
+
+//lokasi berhasil di akses
+function success(position) {
+  console.log(position);
+}
+
+//halaman index
 function index() {
   let app = document.getElementById("app");
   let h3 = document.createElement("h3");
   h3.innerHTML = "Prayer Times";
 
   app.appendChild(h3);
+
+  //pemanggilan function lokasi
+  userLocation();
 }
 
 index();
